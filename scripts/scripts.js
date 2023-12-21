4e481bb9eb4b84e3057bb9bd91b0d4136fef17ed
shubin.andrej <haxapaktepe@gmail.com>
2023-12-21 18:20:46 +0400

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,30 +4,32 @@ $(document).ready(function () {
   const menu = document.querySelector('.menu')
   const menuItem = document.querySelectorAll('.menu__item')
 
-  if (burger) {
-    $(document).on('click', function (e) {
-      if (!$(e.target).closest('.active').length) {
-        burger.classList.remove('active')
-        menu.classList.remove('active')
-        body.classList.remove('no-scroll')
-      }
-      e.stopPropagation()
-    })
-
-    menuItem.forEach((item) => {
-      item.addEventListener('click', () => {
-        burger.classList.toggle('active')
-        menu.classList.remove('active')
-        body.classList.remove('no-scroll')
-      })
-    })
-
-    burger.addEventListener('click', () => {
-      burger.classList.toggle('active')
-      menu.classList.toggle('active')
-      body.classList.toggle('no-scroll')
-    })
+  const toggleMenu = () => {
+    menu.classList.toggle('menu--active')
+    burger.classList.toggle('burger--active')
+    body.classList.toggle('no-scroll')
   }
+
+  const clickOutsideMenu = (event) => {
+    if (!menu.contains(event.target) && !burger.contains(event.target)) {
+      menu.classList.remove('menu--active')
+      burger.classList.remove('burger--active')
+      body.classList.remove('no-scroll')
+    }
+  }
+
+  burger.addEventListener('click', toggleMenu)
+  document.addEventListener('click', clickOutsideMenu)
+
+  // if (burger) {
+  //   menuItem.forEach((item) => {
+  //     item.addEventListener('click', () => {
+  //       burger.classList.toggle('active')
+  //       menu.classList.remove('active')
+  //       body.classList.remove('no-scroll')
+  //     })
+  //   })
+  // }
 })
 
 if (document.querySelector('[name="phone"]')) {
