54f1aa07d30c75a3126e623c574c58aa4876d666
shubin.andrej <haxapaktepe@gmail.com>
2024-03-29 17:24:20 +0400

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,122 +1,155 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const body = document.querySelector('body')
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const menuItem = document.querySelectorAll('.menu__item')
+	const body = document.querySelector('body')
+	const burger = document.querySelector('.burger')
+	const menu = document.querySelector('.menu')
+	const menuItem = document.querySelectorAll('.menu__item')
 
-  const toggleMenu = () => {
-    menu.classList.toggle('menu--active')
-    burger.classList.toggle('burger--active')
-    body.classList.toggle('no-scroll')
-  }
+	const toggleMenu = () => {
+		menu.classList.toggle('menu--active')
+		burger.classList.toggle('burger--active')
+		body.classList.toggle('no-scroll')
+	}
 
-  const clickOutsideMenu = (event) => {
-    if (!menu.contains(event.target) && !burger.contains(event.target)) {
-      menu.classList.remove('menu--active')
-      burger.classList.remove('burger--active')
-      body.classList.remove('no-scroll')
-    }
-  }
+	const clickOutsideMenu = event => {
+		if (!menu.contains(event.target) && !burger.contains(event.target)) {
+			menu.classList.remove('menu--active')
+			burger.classList.remove('burger--active')
+			body.classList.remove('no-scroll')
+		}
+	}
 
-  burger.addEventListener('click', toggleMenu)
-  document.addEventListener('click', clickOutsideMenu)
+	burger.addEventListener('click', toggleMenu)
+	document.addEventListener('click', clickOutsideMenu)
 
-  // if (burger) {
-  //   menuItem.forEach((item) => {
-  //     item.addEventListener('click', () => {
-  //       burger.classList.toggle('active')
-  //       menu.classList.remove('active')
-  //       body.classList.remove('no-scroll')
-  //     })
-  //   })
-  // }
+	// if (burger) {
+	//   menuItem.forEach((item) => {
+	//     item.addEventListener('click', () => {
+	//       burger.classList.toggle('active')
+	//       menu.classList.remove('active')
+	//       body.classList.remove('no-scroll')
+	//     })
+	//   })
+	// }
 
-  function handleTabClick(tabs, pages, activeTabClass, activePageClass, opacityPageClass) {
-    tabs.forEach((tab, idx) => {
-      tab.addEventListener('click', () => {
-        tabs.forEach((tab) => tab.classList.remove(activeTabClass))
-        pages.forEach((page) => {
-          page.classList.remove(activePageClass)
-          page.classList.remove(opacityPageClass)
-        })
+	function handleTabClick(
+		tabs,
+		pages,
+		activeTabClass,
+		activePageClass,
+		opacityPageClass
+	) {
+		tabs.forEach((tab, idx) => {
+			tab.addEventListener('click', () => {
+				tabs.forEach(tab => tab.classList.remove(activeTabClass))
+				pages.forEach(page => {
+					page.classList.remove(activePageClass)
+					page.classList.remove(opacityPageClass)
+				})
 
-        tab.classList.add(activeTabClass)
-        pages[idx].classList.add(activePageClass)
+				tab.classList.add(activeTabClass)
+				pages[idx].classList.add(activePageClass)
 
-        setTimeout(() => {
-          pages[idx].classList.add(opacityPageClass)
-        }, 380)
-      })
-    })
-  }
+				setTimeout(() => {
+					pages[idx].classList.add(opacityPageClass)
+				}, 380)
+			})
+		})
+	}
 
-  const tabs = document.querySelectorAll('.tab__target')
-  const pages = document.querySelectorAll('.tab__info')
+	const tabs = document.querySelectorAll('.tab__target')
+	const pages = document.querySelectorAll('.tab__info')
 
-  handleTabClick(tabs, pages, 'tab__target--active', 'tab__info--active', 'tab__info--opacity')
+	handleTabClick(
+		tabs,
+		pages,
+		'tab__target--active',
+		'tab__info--active',
+		'tab__info--opacity'
+	)
 
-  // const accordion = document.querySelectorAll('.accordion')
-  // accordion?.forEach((acc) => {
-  //   acc.addEventListener('click', (e) => {
-  //     e.preventDefault()
-  //     // const content = acc.querySelector('.accordion__content')
-  //     const content = acc.nextElementSibling
-  //     if (acc.classList.contains('accordion--active')) {
-  //       acc.classList.remove('accordion--active')
-  //       content.style.maxHeight = '0'
-  //     } else {
-  //       acc.classList.add('accordion--active')
-  //       content.style.maxHeight = content.scrollHeight + 'px'
-  //     }
-  //   })
-  // })
-  const accordions = document.querySelectorAll('.accordion')
-  const contents = document.querySelectorAll('.accordion-content')
+	// const accordion = document.querySelectorAll('.accordion')
+	// accordion?.forEach((acc) => {
+	//   acc.addEventListener('click', (e) => {
+	//     e.preventDefault()
+	//     // const content = acc.querySelector('.accordion__content')
+	//     const content = acc.nextElementSibling
+	//     if (acc.classList.contains('accordion--active')) {
+	//       acc.classList.remove('accordion--active')
+	//       content.style.maxHeight = '0'
+	//     } else {
+	//       acc.classList.add('accordion--active')
+	//       content.style.maxHeight = content.scrollHeight + 'px'
+	//     }
+	//   })
+	// })
+	const accordions = document.querySelectorAll('.accordion')
+	const contents = document.querySelectorAll('.accordion-content')
 
-  accordions?.forEach((acc, index) => {
-    acc.addEventListener('click', (e) => {
-      e.preventDefault()
+	accordions?.forEach((acc, index) => {
+		acc.addEventListener('click', e => {
+			e.preventDefault()
 
-      const content = contents[index]
+			const content = contents[index]
 
-      if (acc.classList.contains('accordion--active')) {
-        acc.classList.remove('accordion--active')
-        content.style.maxHeight = '0'
-      } else {
-        acc.classList.add('accordion--active')
-        content.style.maxHeight = content.scrollHeight + 'px'
-      }
-    })
-  })
+			if (acc.classList.contains('accordion--active')) {
+				acc.classList.remove('accordion--active')
+				content.style.maxHeight = '0'
+			} else {
+				acc.classList.add('accordion--active')
+				content.style.maxHeight = content.scrollHeight + 'px'
+			}
+		})
+	})
 
-  const count = document.querySelectorAll('.count')
+	const count = document.querySelectorAll('.count')
 
-  count?.forEach((element) => {
-    element.addEventListener('click', function (event) {
-      const e = event.target
-      const num = element.querySelector('.count__num')
-      let sum = +num.innerHTML
+	count?.forEach(element => {
+		element.addEventListener('click', function (event) {
+			const e = event.target
+			const num = element.querySelector('.count__num')
+			let sum = +num.innerHTML
 
-      if (e.classList.contains('count__plus')) {
-        ++sum
-        num.innerHTML = sum
-      }
-      if (e.classList.contains('count__minus')) {
-        if (sum > 1) {
-          --sum
-          num.innerHTML = sum
-        }
-      }
-    })
-  })
+			if (e.classList.contains('count__plus')) {
+				++sum
+				num.innerHTML = sum
+			}
+			if (e.classList.contains('count__minus')) {
+				if (sum > 1) {
+					--sum
+					num.innerHTML = sum
+				}
+			}
+		})
+	})
 
-  if (document.querySelector('[name="phone"]')) {
-    const element = document.querySelector('[name="phone"]')
-    const maskOptions = {
-      mask: '+{7} 000 000 00 00',
-    }
-    const mask = IMask(element, maskOptions)
-  }
+	const menuLinks = document.querySelectorAll('[class][data-goto]')
+
+	if (menuLinks.length > 0) {
+		menuLinks.forEach(link => {
+			link.addEventListener('click', onMenuLinkClick)
+		})
+
+		function onMenuLinkClick(e) {
+			const link = e.target
+			if (link.dataset.goto && document.querySelector(link.dataset.goto)) {
+				const gotoBlock = document.querySelector(link.dataset.goto)
+				const gotoBlockValue = gotoBlock.getBoundingClientRect().top
+				window.scrollBy({
+					top: gotoBlockValue,
+					behavior: 'smooth',
+				})
+				e.preventDefault()
+			}
+		}
+	}
+
+	if (document.querySelector('[name="phone"]')) {
+		const element = document.querySelector('[name="phone"]')
+		const maskOptions = {
+			mask: '+{7} 000 000 00 00',
+		}
+		const mask = IMask(element, maskOptions)
+	}
 })
 // $(document).ready(function () {})
 // $('.catalog__sorting').select2({
